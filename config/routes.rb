2278407deedddf776865e82565c82af5f82ba701
Lee Stetson <stetsonthree@icloud.com>
2020-07-30 19:36:50 -0400

    Sets up Project, Task scaffolding, basics
    - Adds devise gem to create User
    - Adds nested_scaffold gem
    - Creates project/task structure
    - Basic controller/view changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :projects do
+    resources :tasks
+  end
+  devise_for :users
+  root to: "projects#index"
 end
