e40dcee4d8c7eba35072688fe068dd36a27ece16
Lee Stetson <stetsonthree@icloud.com>
2020-08-07 20:14:44 -0400

    Refactors component code
    - Refactors component code

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,29 +9,6 @@ class Task < ApplicationRecord
     ['Complete', 'complete']
   ]
 
-  def color_class
-    case status
-    when 'not-started'
-      'secondary'
-    when 'in-progress'
-      'info'
-    when 'complete'
-      'success'
-    end
-  end
-
-  def readable_status
-    case status
-    when 'not-started'
-      'Not started'
-    when 'in-progress'
-      'In progress'
-    when 'complete'
-      'Complete'
-    end
-  end
-  
-
   def complete?
     status == 'complete'
   end
